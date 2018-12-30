a5c19cf28aee20b844eaa4076851bcc5916171fd
lanzhiheng <lanzhihengrj@gmail.com>
2018-12-30 23:09:55 +0800

    feat: Add some shortcut and some comments.

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,14 +1,29 @@
+;;; init-local.el --- Custom some behavior for myself.
+;;; Commentary:
+;;; Code:
+
+;; Markdown
 (setq markdown-command "/usr/local/bin/pandoc")
 
+;; For Adjusting window's size
 (global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
 (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "S-C-<down>") 'shrink-window)
 (global-set-key (kbd "S-C-<up>") 'enlarge-window)
 
+;; For system paste
+(global-set-key (kbd "M-v") 'cua-paste)
+
+;; for multi line edit
+(global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
+
 (setq url-proxy-services
       '(("http"     . "localhost:8118")
         ("https"    . "localhost:8118")))
 
+
+;; Alway use option as meta
 (setq mac-option-modifier 'meta)
 
 (provide 'init-local)
+;;; init-local.el ends here
