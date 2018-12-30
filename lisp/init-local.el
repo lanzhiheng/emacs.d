f37c988becd5422737a5e0e022bf2483ba88b504
lanzhiheng <lanzhihengrj@gmail.com>
2018-12-30 09:53:13 +0800

    feat: Add keybinding for adjust the window size.

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,5 +1,10 @@
 (setq markdown-command "/usr/local/bin/pandoc")
 
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>") 'shrink-window)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
+
 (setq url-proxy-services
       '(("http"     . "localhost:8118")
         ("https"    . "localhost:8118")))
