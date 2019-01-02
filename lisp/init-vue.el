c9769f1c9f73bfca33e56a82c856a8d4bcbd1e68
lanzhiheng <lanzhihengrj@gmail.com>
2019-01-02 13:10:55 +0800

    feat: Add vue mode.

---
+++ lisp/init-vue.el
@@ -0,0 +1,10 @@
+;;; init-vue.el --- The vue mode configuration
+;;; Commentary:
+;;; Code:
+
+(require-package 'vue-mode)
+
+(add-auto-mode 'vue-mode "\\.vue\\'")
+
+(provide 'init-vue)
+;;; init-vue.el ends here
